68afc8e98b83a448fc0c2b1bd5909b11990607c9
Prakash Goud <prakash.goud@morningstar.com>
2021-09-21 00:19:40 +0530

    [Prakash] - Added the ability to check connection and retrive entries from database.

---
+++ lib/src/main/java/com/bridgelabz/addressbooksystem/main/AddressBookService.java
@@ -0,0 +1,49 @@
+package com.bridgelabz.addressbooksystem.main;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class AddressBookService {
+
+	public boolean checkConnection(String URL, String userName, String password) throws SQLException {
+
+		try {
+			Connection connection = DriverManager.getConnection(URL, userName, password);
+			System.out.println("Connection eastablished with data base.");
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	public String getDataFromTables(Connection connection, String tableName, String query) throws SQLException {
+		try {
+			DatabaseMetaData db = connection.getMetaData();
+			ResultSet tables = db.getTables(null, null, "address_book_table", null);
+			Statement stm = connection.createStatement();
+			while (tables.next()) {
+				String table = tables.getString("TABLE_NAME");
+				if (table.equals(tableName)) {
+					ResultSet resultSet = stm.executeQuery(query);
+					while (resultSet.next()) {
+						System.out.println(resultSet.getString(1) + " " + resultSet.getString(2) + " "
+								+ resultSet.getString(3) + " " + resultSet.getString(4) + " " + resultSet.getString(5)
+								+ " " + resultSet.getString(6) + " " + resultSet.getInt(7) + " " + resultSet.getInt(8)
+								+ " " + resultSet.getString(9));
+					}
+					return "Data is printed";
+				}
+				continue;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return "Table is not present in data base";
+	}
+
+}
---
+++ lib/src/test/java/com/bridgelabz/addressbooksystem/test/AddressBookServiceTest.java
@@ -0,0 +1,42 @@
+package com.bridgelabz.addressbooksystem.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.bridgelabz.addressbooksystem.main.AddressBookService;
+
+public class AddressBookServiceTest {
+
+	public static String jdbcURL = "jdbc:mysql://localhost:3306/address_book_service?useSSL=false";
+	public static String userName = "root";
+	public static String password = "mysql123@";
+	public static String query = "Select * from address_book_table;";
+	AddressBookService service = new AddressBookService();
+
+	@Test
+	public void checkConnectionWithDBisEastablished() throws SQLException {
+		try {
+			boolean result = service.checkConnection(jdbcURL, userName, password);
+			Assert.assertEquals(true, result);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void retriveDataFromTable_OrElseCreateATable() throws SQLException {
+		try {
+			Connection connection = DriverManager.getConnection(jdbcURL, userName, password);
+			String result = service.getDataFromTables(connection, "address_book_table", query);
+			System.out.println(result);
+			Assert.assertEquals("Data is printed", result);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
